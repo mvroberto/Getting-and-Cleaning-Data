481901e77112391588982f5092395442ef1a3838
mvroberto <mvalle.roberto@gmail.com>
2016-10-11 20:27:26 -0500

    Update quizzTwo.R

--- quizzTwo.R
+++ quizzTwo.R
@@ -38,9 +38,60 @@ answer_two <- function(){
 }
 
 
-answer_three <- function(){
+answer_four <- function(){
 	library(XML)
-	fileUrl <- "http://biostat.jhsph.edu/~jleek/contact.html"
-	doc <- htmlTreeParse(fileUrl)
-	doc[[1]][[10]]
+	library(httr)
+	library(sqldf)
+	
+	date_downloaded <- date()
+	directory_files <- dir()
+	if(!is.element("questionFour.html",directory_files)){
+		fileUrl <- "http://biostat.jhsph.edu/~jleek/contact.html"
+		download.file(fileUrl, destfile ="/Users/robertovallemateos/Desktop/gitHub_excersices/gettingData/questionFour.html")
+	}
+	
+	readed_html <- htmlParse("/Users/robertovallemateos/Desktop/gitHub_excersices/gettingData/questionFour.html", useInternal = TRUE)
+	my_file <- capture.output(readed_html)
+	my_file
+	
+	file_lenght <- c(nchar(my_file[8]),nchar(my_file[20]),nchar(my_file[30]),nchar(my_file[100]))
+	my_file
+	
+	################## Works Better :)	
+	make_connection <- url("http://biostat.jhsph.edu/~jleek/contact.html")
+        htmlCode <- readLines(make_connection)
+	close(make_connection)
+	real_list <- c(nchar(htmlCode[10]),nchar(htmlCode[20]),nchar(htmlCode[30]),nchar(htmlCode[100]))
+	
+	my_list <- list(my_file,htmlCode)
+	
+	file_lenght
+	my_list
+	real_list
+	
+}
+
+a <- function(){
+	library(XML)
+	library(httr)
+	
+	make_connection <- url("http://biostat.jhsph.edu/~jleek/contact.html")
+	htmlCode <- readLines(make_connection)
+	close(make_connection)
+	return(c(nchar(htmlCode[10]), nchar(htmlCode[20]), nchar(htmlCode[30]), nchar(htmlCode[100])))
+}
+
+answer_five <- function(){
+	library(httr)
+	library(sqldf)
+	
+	date_downloaded <- date()
+	directory_files <- dir()
+	if(!is.element("questionFive.for",directory_files)){
+		fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fwksst8110.for"
+		download.file(fileUrl, destfile ="/Users/robertovallemateos/Desktop/gitHub_excersices/gettingData/questionFive.for")
+	}
+	
+	my_file <- read.fwf("questionFive.for", skip = 4, widths = c(12,7,4,9,4,9,4))
+	sum(my_file[,4])
 }
